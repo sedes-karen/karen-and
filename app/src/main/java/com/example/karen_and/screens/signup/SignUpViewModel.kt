fad32ff628bea12e615dcced8e3c8ed21f027de2
GiulianaMercadoAviles <giulianamercado43@gmail.com>
2025-10-23 19:51:28 -0300

    Pantallas de login y de registro

---
+++ app/src/main/java/com/example/karen_and/screens/signup/SignUpViewModel.kt
@@ -0,0 +1,65 @@
+package com.example.karen_and.screens.login
+
+import android.util.Patterns
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.karen_and.ui.ui_events.UIEvents
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import com.example.karen_and.network.LoginService
+
+class LoginViewModel : ViewModel() {
+    private val _state = MutableStateFlow(LoginState())
+    val state: StateFlow<LoginState> = _state
+    private val _events = MutableSharedFlow<UIEvents>()
+    val events = _events.asSharedFlow()
+
+    fun onEmailChange(newEmail: String) {
+        _state.update {
+            val updated = it.copy(email = newEmail)
+            updated.copy(isFormValid = validate(updated))
+        }
+    }
+
+    fun onPasswordChange(newPass: String) {
+        _state.update {
+            val updated = it.copy(password = newPass)
+            updated.copy(isFormValid = validate(updated))
+        }
+    }
+
+    fun submit() {
+        if (_state.value.isFormValid) {
+            viewModelScope.launch {
+                _state.update { it.copy(isLoading = true) }
+
+                val result = LoginService.login(_state.value.email, _state.value.password)
+
+                result
+                    .onSuccess {
+                        _events.emit(UIEvents.ShowSnackbar("Sesión iniciada correctamente"))
+                    }
+                    .onFailure { error ->
+                        _events.emit(UIEvents.ShowSnackbar(error.message ?: "Ocurrió un error"))
+                    }
+
+                _state.update { it.copy(isLoading = false) }
+            }
+        } else {
+            viewModelScope.launch {
+                _events.emit(UIEvents.ShowSnackbar("Email o contraseña incorrectas. Por favor, revisar los campos"))
+            }
+        }
+    }
+
+    private fun validate(state: LoginState): Boolean {
+        val emailOk = Patterns.EMAIL_ADDRESS.matcher(state.email).matches()
+        val passOk = state.password.length >= 6
+        return emailOk && passOk && !state.isLoading
+    }
+
+}
