f1b7b1452ee877be394168bac8289c2de024c66c
Juan Cruz Vila <parlantephilips7@gmail.com>
2025-10-30 13:06:54 -0300

    fix: build errors

--- app/src/main/java/com/example/karen_and/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/karen_and/screens/login/LoginScreen.kt
@@ -1,7 +1,4 @@
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
